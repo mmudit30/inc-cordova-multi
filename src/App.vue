75c6aecd694f4274a08c53afcab5cff496865b24
Abhishek-Kathayat <abhishekkathayat@outlook.com>
2018-09-06 19:41:42 +0530

    Added Login component

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,19 @@
 <template>
   <v-app light>
-    <v-navigation-drawer
-      fixed
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon light v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-toolbar fixed app :clipped-left="clipped">
-      <v-toolbar-side-icon @click.stop="drawer = !drawer" light></v-toolbar-side-icon>
-      <v-btn
-        icon
-        light
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        light
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-title v-text="title" text-xs-center></v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
-        icon
-        light
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
-      </v-btn>
+      outline color="blue"
+      :onClick="consoleClick"
+      >Help</v-btn>
     </v-toolbar>
     <v-content>
       <router-view></router-view>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
   </v-app>
 </template>
-
 <script>
   import Vue from 'vue'
   export default {
@@ -77,15 +21,8 @@
       return {
         cordova: Vue.cordova,
         clipped: false,
-        drawer: true,
-        items: [{
-          icon: 'bubble_chart',
-          title: 'Inspire'
-        }],
         miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'Vuetify.js'
+        title: 'Aapki Sevamen'
       }
     },
     created () {
---
+++ src/components/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-layout>
+        <v-flex>
+          <br/><br/>
+          <v-text-field
+            ref = "email"
+            v-model = "email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field><br/>
+          <v-text-field
+            v-model="password"
+            :append-icon="show1 ? 'visibility_off' : 'visibility'"
+            :rules="[rules.required, rules.min]"
+            :type="show1 ? 'text' : 'password'"
+            name="input-10-1"
+            label="Password"
+            hint="At least 8 characters"
+            counter
+            @click:append="show1 = !show1"
+          ></v-text-field>
+          <br/><br/><br/>
+          <v-btn large block depressed color="primary">Log in</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        email: '',
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+/.test(v) || 'E-mail must be valid'
+        ],
+        password: '',
+        show1: true,
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Minimum 8 characters',
+          emailMatch: () => ('The email and password you entered don\'t match')
+        }
+      }
+    }
+  }
+</script>
