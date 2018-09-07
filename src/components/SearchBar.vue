a291a7fe38929097cd8a6971fda7e7dbc06805db
Abhishek-Kathayat <abhishekkathayat@outlook.com>
2018-09-07 23:01:14 +0530

    Added Product List and Bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <v-text-field
+          placeholder="Search for a service"
+          hint="For example cab, grocery, medicines"
+          append-icon="search"
+          type="text"
+          outline
+          single-line
+          color="primary"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
